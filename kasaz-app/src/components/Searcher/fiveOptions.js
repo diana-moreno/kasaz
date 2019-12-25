4220defa4ea330b66fbff36f5a43738f6ea5a361
Diana <d7@hotmail.es>
2019-12-25 02:34:41 +0100

    separate constants in a different file

--- kasaz-app/src/components/Searcher/fiveOptions.js
+++ kasaz-app/src/components/Searcher/fiveOptions.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-
 export default function ({ description, values }) {
 
   useEffect(() => {
--- kasaz-app/src/components/Searcher/listOptions.js
+++ kasaz-app/src/components/Searcher/listOptions.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
+import constants from './constants'
+const { prices,  sqms } = constants
 export default function ({ description, isSqm, isPrice }) {
-
-const prices = [0, 25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 225000, 250000, 275000, 300000, 325000, 350000, 375000, 400000, 325000, 350000, 400000, 450000, 500000, 550000, 600000, 650000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1500000]
-
-const sqms= [0, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,150, 200, 300]
 
   useEffect(() => {
   }, [])
