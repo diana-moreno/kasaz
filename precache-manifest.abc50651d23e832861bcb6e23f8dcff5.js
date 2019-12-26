cfecc8d7b63f3847bc571de654aa5ebaceb84dae
Diana <d7@hotmail.es>
2019-12-26 19:55:13 +0100

    Updates

---
+++ precache-manifest.abc50651d23e832861bcb6e23f8dcff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb8ec8124429711bd7b006b6135add0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b7de5f51f2240171aa4",
+    "url": "./static/css/main.66c293a2.chunk.css"
+  },
+  {
+    "revision": "d0ebf0fa000a6ebde199",
+    "url": "./static/js/2.2e71ca68.chunk.js"
+  },
+  {
+    "revision": "4b7de5f51f2240171aa4",
+    "url": "./static/js/main.a168988a.chunk.js"
+  },
+  {
+    "revision": "5c41ecd7fcc878ef3893",
+    "url": "./static/js/runtime-main.8f971e7c.js"
+  }
+]);
