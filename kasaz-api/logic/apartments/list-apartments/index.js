c6819d2d55c7eed932c3f1dcf2192fef744b7612
Diana <d7@hotmail.es>
2019-12-27 11:39:52 +0100

    refactor backend logic

--- kasaz-api/logic/apartments/list-apartments/index.js
+++ kasaz-api/logic/apartments/list-apartments/index.js
@@ -12,32 +12,36 @@ module.exports = function(location, minPrice, maxPrice, minSqm, maxSqm, bedrooms
   bedrooms && validate.number(bedrooms)
   bathrooms && validate.number(bathrooms)
 
+  // set default values if undefined is received
+  if (!location) location = 'Barcelona'
+  if (!minPrice) minPrice = 0
+  if (!maxPrice) maxPrice = Number.MAX_SAFE_INTEGER
+  if (!minSqm) minSqm = 0
+  if (!maxSqm) maxSqm = Number.MAX_SAFE_INTEGER
+
   return (async () => {
+    let initialQuery = {
+      location: { $regex: location, $options: 'i' },
+      price: { $gte: minPrice, $lte: maxPrice },
+      sqm: { $gte: minSqm, $lte: maxSqm }
+    }
 
-    // set default values if undefined is received
-    if (!location) location = 'Barcelona'
-    if (!minPrice) minPrice = 0
-    if (!maxPrice) maxPrice = Number.MAX_SAFE_INTEGER
-    if (!minSqm) minSqm = 0
-    if (!maxSqm) maxSqm = Number.MAX_SAFE_INTEGER
-
-    let apartments
-
-    apartments = await Apartment.find({ location: { $regex: location, $options: 'i' }, price: { $gte: minPrice, $lte: maxPrice }, sqm: { $gte: minSqm, $lte: maxSqm } }, { __v: 0 }).lean()
+    let findParams = []
 
     if (bedrooms && !bathrooms) {
-      apartments = await Apartment.find({ location: { $regex: location, $options: 'i' }, price: { $gte: minPrice, $lte: maxPrice }, sqm: { $gte: minSqm, $lte: maxSqm }, bedrooms }, { __v: 0 }).lean()
+      findParams.push({ ...initialQuery, bedrooms }, { __v: 0 })
+    } else if (bathrooms && !bedrooms) {
+      findParams.push({ ...initialQuery, bathrooms }, { __v: 0 })
+    } else if (bathrooms && bedrooms) {
+      findParams.push({ ...initialQuery, bathrooms, bedrooms }, { __v: 0 })
+    } else {
+      findParams.push({ ...initialQuery }, { __v: 0 })
     }
 
-    if (bathrooms && !bedrooms) {
-      apartments = await Apartment.find({ location: { $regex: location, $options: 'i' }, price: { $gte: minPrice, $lte: maxPrice }, sqm: { $gte: minSqm, $lte: maxSqm }, bathrooms }, { __v: 0 }).lean()
-    }
+    const apartments = await Apartment.find(...findParams).lean()
 
-    if (bathrooms && bedrooms) {
-      apartments = await Apartment.find({ location: { $regex: location, $options: 'i' }, price: { $gte: minPrice, $lte: maxPrice }, sqm: { $gte: minSqm, $lte: maxSqm }, bedrooms, bathrooms }, { __v: 0 }).lean()
-    }
-
-    if (apartments.length === 0) throw new NotFoundError(`Lo sentimos, no hemos encontrado resultados.`)
+    if (apartments.length === 0)
+      throw new NotFoundError(`Lo sentimos, no hemos encontrado resultados.`)
 
     return apartments
   })()
--- kasaz-api/routes/apartments/index.js
+++ kasaz-api/routes/apartments/index.js
@@ -12,12 +12,8 @@ router.get('/', (req, res) => {
       .then(apartments => res.json({ apartments }))
       .catch(error => {
         const { message } = error
-
         if (error instanceof NotFoundError)
           return res.status(404).json({ message })
-        if (error instanceof ConflictError)
-          return res.status(409).json({ message })
-
         res.status(500).json({ message })
       })
   } catch ({ message }) {
